5c459da2947fd2031d81010e35a8a3a2448ac2b0
iammuqeet <iammuqeetahmad@gmail.com>
2023-10-11 14:34:28 +0500

    Added the UI for the Text project

--- src/App.js
+++ src/App.js
@@ -1,33 +1,366 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import BubbleView from 'components/BubbleView/BubbleView'
-import { fetchAllCompanies } from 'api/company'
-import { createLinks } from 'utils/helpers'
-import { UPDATE_COMPANIES } from 'store/companies'
+import { RiFilter2Line } from "react-icons/ri";
+import { FiSearch } from "react-icons/fi";
+
+import {
+  Layout,
+  Input,
+  Row,
+  Col,
+  Avatar,
+  Dropdown,
+  Menu,
+  Select,
+  Space,
+} from "antd";
+
+import {
+  BellOutlined,
+  MessageOutlined,
+  DownOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+
+import BubbleView from "components/BubbleView/BubbleView";
+import { fetchAllCompanies } from "api/company";
+import { createLinks } from "utils/helpers";
+import { UPDATE_COMPANIES } from "store/companies";
+
+const { Header, Content, Footer } = Layout;
+const { Option } = Select;
 
 const App = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const [selectedKeys, setSelectedKeys] = useState(["hall"]);
+
+  const handleMenuClick = (e) => {
+    setSelectedKeys([e.key]);
+  };
 
   const getAllCompanies = async () => {
-    const responce = await fetchAllCompanies()
-    console.log(responce)
-    const updatedData = createLinks(responce)
-    dispatch(UPDATE_COMPANIES(updatedData))
-  }
+    try {
+      const response = await fetchAllCompanies();
+      console.log(response);
+      const updatedData = createLinks(response);
+      dispatch(UPDATE_COMPANIES(updatedData));
+    } catch (error) {
+      console.error("Error fetching companies:", error);
+    }
+  };
 
   useEffect(() => {
-    getAllCompanies()
-  }, [])
+    getAllCompanies();
+  }, []);
+
+  const styles = {
+    container: {
+      padding: "0px 40px 0px 40px",
+      background: "#F0F0F0",
+    },
+
+    header: {
+      background: "#F0F0F0",
+      padding: "10px 0px",
+    },
+
+    logo: {
+      display: "flex",
+      alignItems: "center",
+    },
+
+    logoText: {
+      marginRight: "10px",
+      color: "#808080",
+    },
+
+    search: {
+      width: "300px",
+      marginRight: "10px",
+    },
+
+    rightContent: {
+      textAlign: "right",
+    },
+
+    icon: {
+      fontSize: "18px",
+      marginRight: "20px",
+    },
+
+    profileLink: {
+      color: "#808080",
+    },
+
+    profileName: {
+      marginLeft: "8px",
+      color: "#808080",
+    },
+
+    menuRow: {
+      padding: "0 0 10px 0",
+      background: "#F0F0F0",
+    },
+
+    menu: {
+      borderBottomColor: "#ff7c16",
+    },
+
+    menuItem: {
+      fontWeight: "bold",
+      color: "#000",
+      background: "#F0F0F0",
+      padding: "0px",
+    },
+
+    menuItemText: {
+      borderBottom: "2px solid #FF681A",
+      paddingBottom: "16px",
+    },
+
+    content: {
+      background: "#fff",
+      borderRadius: "5px",
+    },
+
+    searchCol: {
+      display: "flex",
+      alignItems: "center",
+    },
+
+    searchInput: {
+      width: "300px",
+      padding: "20px",
+    },
+
+    filterCol: {
+      display: "flex",
+      alignItems: "center",
+    },
+
+    filterContainer: {
+      display: "flex",
+      alignItems: "center",
+      border: "1px solid #CED6E0",
+      borderRadius: "5px",
+      padding: "4px",
+    },
+
+    filterIcon: {
+      fontSize: "18px",
+      paddingRight: "5px",
+    },
+
+    filterText: {
+      marginRight: "5px",
+    },
+
+    filterCount: {
+      display: "inline-block",
+      height: "18px",
+      width: "18px",
+      background: "#F2F5FA",
+      borderTopRightRadius: "5px",
+      borderBottomLeftRadius: "5px",
+      textAlign: "center",
+      lineHeight: "18px",
+      fontSize: "12px",
+      color: "#24292E",
+    },
+
+    footer: {
+      textAlign: "center",
+      background: "#f0f0f0",
+      padding: "20px 0 20px 0",
+    },
+
+    footerText: {
+      color: "#A0A0A0",
+      paddingRight: "20px",
+    },
+  };
+
+  const filterCount = 7; // Update this based on the actual filter count
+
+  const Navbar = () => {
+    const menu = (
+      <Menu>
+        <Menu.Item key="profile">Profile</Menu.Item>
+        <Menu.Item key="logout">Logout</Menu.Item>
+      </Menu>
+    );
+
+    return (
+      <Header style={styles.header}>
+        <Row justify="space-between" align="middle">
+          <Col span={8} style={styles.logo}>
+            <span style={styles.logoText}>LOGO</span>
+            <Input.Search placeholder="Search" style={styles.search} />
+          </Col>
+          <Col span={8} style={styles.rightContent}>
+            <BellOutlined style={styles.icon} />
+            <MessageOutlined style={styles.icon} />
+            <Dropdown overlay={menu}>
+              <a
+                className="ant-dropdown-link"
+                onClick={(e) => e.preventDefault()}
+              >
+                <Avatar size="medium" icon={<UserOutlined />} />
+                <span style={styles.profileName}>Aladdinb2b LLC</span>{" "}
+                <DownOutlined />
+              </a>
+            </Dropdown>
+          </Col>
+        </Row>
+      </Header>
+    );
+  };
 
   return (
     <Router>
-      <Routes>
-        <Route exact path='/' element={<BubbleView />} />
-      </Routes>
-    </Router>
-  )
-}
+      <Layout style={styles.container}>
+        <Navbar />
+        {/* <Row justify="start" style={styles.menuRow}>
+          <Col>
+            <Menu
+              mode="horizontal"
+              selectedKeys={selectedKeys}
+              onClick={handleMenuClick}
+              style={styles.menu}
+            >
+              <Menu.Item key="hall" style={styles.menuItem}>
+                <span style={styles.menuItemText}>Hall</span>
+              </Menu.Item>
+              <Menu.Item key="matches" style={styles.menuItem}>
+                Matches
+              </Menu.Item>
+              <Menu.Item key="meeting" style={styles.menuItem}>
+                Meeting
+              </Menu.Item>
+              <Menu.Item key="tradehub" style={styles.menuItem}>
+                Trade Hub
+              </Menu.Item>
+              <Menu.Item key="pipeline" style={styles.menuItem}>
+                Pipeline
+              </Menu.Item>
+            </Menu>
+          </Col>
+        </Row> */}
 
-export default App
+        <Row justify="start" style={styles.menuRow}>
+          <Col>
+            <Menu
+              mode="horizontal"
+              selectedKeys={selectedKeys}
+              onClick={handleMenuClick}
+              style={{
+                borderBottomColor: "#ff7c16",
+              }}
+            >
+              <Menu.Item
+                key="hall"
+                style={{
+                  fontWeight: "bold",
+                  color: "#000",
+                  background: "#F0F0F0",
+                  padding: "0px",
+                }}
+              >
+                <span
+                  style={{
+                    borderBottom: "2px solid #FF681A",
+                    paddingBottom: "16px",
+                  }}
+                >
+                  Hall
+                </span>
+              </Menu.Item>
+              <Menu.Item
+                key="matches"
+                style={{ background: "#F0F0F0", paddingRight: "10px" }}
+              >
+                Matches
+              </Menu.Item>
+              <Menu.Item
+                key="meeting"
+                style={{ background: "#F0F0F0", paddingRight: "10px" }}
+              >
+                Meeting
+              </Menu.Item>
+              <Menu.Item
+                key="tradehub"
+                style={{ background: "#F0F0F0", paddingRight: "10px" }}
+              >
+                Trade Hub
+              </Menu.Item>
+              <Menu.Item
+                key="pipeline"
+                style={{ background: "#F0F0F0", paddingRight: "10px" }}
+              >
+                Pipeline
+              </Menu.Item>
+            </Menu>
+          </Col>
+        </Row>
+
+        <Content style={styles.content}>
+          <Row justify="start" align="middle">
+            <Col style={styles.searchCol}>
+              <Input.Search placeholder="Search" style={styles.searchInput} />
+            </Col>
+
+            <Col style={styles.filterCol}>
+              <div style={styles.filterContainer}>
+                <RiFilter2Line style={styles.filterIcon} />
+                <span style={styles.filterText}>Filter</span>
+                {filterCount > 0 && (
+                  <span style={styles.filterCount}>{filterCount}</span>
+                )}
+              </div>
+            </Col>
+          </Row>
+
+          <Routes>
+            <Route exact path="/" element={<BubbleView />} />
+          </Routes>
+        </Content>
+        <Footer style={styles.footer}>
+          {/* <Row justify="start">
+            <Col>
+              <span style={styles.footerText}>
+                Copy Right © Aladdinb2b - Connection Businesses
+              </span>
+            </Col>
+            <Col>
+              <span style={styles.footerText}>Terms of Use</span>
+            </Col>
+            <Col>
+              <span style={styles.footerText}>Privacy Policy</span>
+            </Col>
+          </Row> */}
+          <Row justify="start">
+            <Col>
+              <span style={{ color: "#A0A0A0", paddingRight: "20px" }}>
+                Copy Right © Aladdinb2b - Connection Businesses
+              </span>
+            </Col>
+            <Col>
+              <span style={{ fontWeight: "bold", paddingRight: "20px" }}>
+                Terms of Use
+              </span>
+            </Col>
+            <Col>
+              <span style={{ fontWeight: "bold", paddingRight: "20px" }}>
+                Privacy Policy
+              </span>
+            </Col>
+          </Row>
+        </Footer>
+      </Layout>
+    </Router>
+  );
+};
+
+export default App;
--- src/components/BubbleView/BubbleView.js
+++ src/components/BubbleView/BubbleView.js
@@ -1,108 +1,261 @@
-// BubbleView.js
-import React, { useEffect, useRef } from 'react'
-import * as d3 from 'd3'
-import './BubbleView.css'
-import { useSelector } from 'react-redux'
-import { cloneDeep } from 'lodash'
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import * as d3 from "d3";
+import "./BubbleView.css";
+import { useSelector } from "react-redux";
+import { cloneDeep } from "lodash";
+import { Modal, Typography, Image, Flex, Button, Avatar, Space } from "antd";
+
+const { Text, Title } = Typography;
 
 const BubbleView = () => {
-  const updatedCompanies = useSelector((state) => state.companies)
-  const updatedCompaniesList = cloneDeep(updatedCompanies)
+  const updatedCompanies = useSelector((state) => state.companies);
+  const updatedCompaniesList = cloneDeep(updatedCompanies);
 
-  const svgRef = useRef()
-  const width = window.innerWidth
-  const height = window.innerHeight
+  useEffect(() => {
+    console.log("updatedCompaniesList ----------->: ", updatedCompaniesList);
+  }, [updatedCompaniesList]);
+
+  const svgRef = useRef();
+  const width = window.innerWidth;
+  const height = Math.min(window.innerHeight, 0.5 * window.innerHeight);
+
+  const [selectedBubbleIndex, setSelectedBubbleIndex] = useState(null);
+
+  const handleBubbleClick = (index) => {
+    console.log("INdex  : ", index);
+    setSelectedBubbleIndex(index);
+  };
 
   useEffect(() => {
     if (updatedCompaniesList.success) {
-      const svg = d3.select(svgRef.current)
+      const svg = d3.select(svgRef.current);
 
       const linkDistance = () => {
-        return Math.floor(Math.random() * 700)
-      }
+        return Math.floor(Math.random() * 700);
+      };
+
+      const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
       const simulation = d3
         .forceSimulation(updatedCompaniesList.result.data)
-        .force('charge', d3.forceManyBody().strength(-200))
-        .force('center', d3.forceCenter(width / 2, height / 2))
-        .force('x', d3.forceX(width / 2).strength(0.01))
-        .force('y', d3.forceY(height / 2).strength(0.06))
+        .force("charge", d3.forceManyBody().strength(-200))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .force("x", d3.forceX(width / 2).strength(0.01))
+        .force("y", d3.forceY(height / 2).strength(0.06))
         .force(
-          'link',
+          "link",
           d3
             .forceLink(updatedCompaniesList.links)
             .id((d) => d._id)
             .distance(linkDistance)
-        )
+        );
 
       const link = svg
-        .selectAll('.link')
+        .selectAll(".link")
         .data(updatedCompaniesList.links)
         .enter()
-        .append('line')
-        .attr('class', 'link')
-        .style('stroke', 'gray')
-        .style('stroke-width', 0.5)
+        .append("line")
+        .attr("class", "link")
+        .style("stroke", "gray")
+        .style("stroke-width", 0.5);
 
       const nodes = svg
-        .selectAll('.node')
+        .selectAll(".node")
         .data(updatedCompaniesList.result.data)
         .enter()
-        .append('g')
-        .attr('class', 'node')
-
-      nodes.append('circle').attr('r', 30).attr('fill', '#3498db')
+        .append("g")
+        .attr("class", "node")
+        .on("click", (d, i) => handleBubbleClick(i));
 
       nodes
-        .append('text')
-        .text((d) => d.name)
-        .attr('text-anchor', 'middle')
-        .attr('dy', 4)
-        .style('font-size', '8px')
+        .append("circle")
+        .attr("r", 30)
+        .attr("fill", (d, i) => colorScale(i));
 
-      simulation.on('tick', () => {
+      nodes
+        .append("text")
+        .text((d) => d.name)
+        .attr("text-anchor", "middle")
+        .attr("dy", 4)
+        .style("font-size", "8px");
+
+      simulation.on("tick", () => {
         nodes.attr(
-          'transform',
+          "transform",
           (d) => `translate(${(d.x = (d.x + width + 2) % (width + 4))},${d.y})`
-        )
+        );
 
         link
-          .attr('x1', (d) => d.source.x)
-          .attr('y1', (d) => d.source.y)
-          .attr('x2', (d) => d.target.x)
-          .attr('y2', (d) => d.target.y)
-      })
+          .attr("x1", (d) => d.source.x)
+          .attr("y1", (d) => d.source.y)
+          .attr("x2", (d) => d.target.x)
+          .attr("y2", (d) => d.target.y);
+      });
 
       return () => {
-        simulation.stop()
-      }
+        simulation.stop();
+      };
     }
-  }, [updatedCompaniesList])
+  }, [updatedCompaniesList]);
 
   useEffect(() => {
     if (updatedCompaniesList.success) {
       const interval = setInterval(() => {
         d3.select(svgRef.current)
-          .selectAll('.node')
-          .attr('transform', (d) => `translate(${(d.x = (d.x + width + 2) % (width + 4))},${d.y})`)
+          .selectAll(".node")
+          .attr(
+            "transform",
+            (d) =>
+              `translate(${(d.x = (d.x + width + 2) % (width + 4))},${d.y})`
+          );
 
         d3.select(svgRef.current)
-          .selectAll('.link')
-          .attr('x1', (d) => d.source.x)
-          .attr('y1', (d) => d.source.y)
-          .attr('x2', (d) => d.target.x)
-          .attr('y2', (d) => d.target.y)
-      }, 30)
+          .selectAll(".link")
+          .attr("x1", (d) => d.source.x)
+          .attr("y1", (d) => d.source.y)
+          .attr("x2", (d) => d.target.x)
+          .attr("y2", (d) => d.target.y);
+      }, 30);
 
-      return () => clearInterval(interval)
+      return () => clearInterval(interval);
     }
-  }, [updatedCompaniesList])
+  }, [updatedCompaniesList, width]);
 
   return (
-    updatedCompaniesList.success && (
-      <svg className='bubble-view' width='100%' height='100%' ref={svgRef}></svg>
-    )
-  )
-}
+    <>
+      {useMemo(
+        () => (
+          <svg
+            className="bubble-view"
+            width="100%"
+            height="100%"
+            ref={svgRef}
+          ></svg>
+        ),
+        []
+      )}
+      {selectedBubbleIndex !== null && (
+        <Modal
+          visible={selectedBubbleIndex !== null}
+          onCancel={() => setSelectedBubbleIndex(null)}
+          footer={null}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              src={selectedBubbleIndex?.logo || "/icons/company_logo.svg"}
+              alt="Company Logo"
+              width={100}
+              height={100}
+              preview={false}
+              fallback="/icons/company_logo.svg"
+              borderRadius="50px"
+            />
+            <Text
+              style={{
+                color: "var(--Cool-Dark-1, #24292E)",
+                textAlign: "center",
+                // fontFamily: 'Montserrat',
+                fontSize: "18px",
+                fontStyle: "normal",
+                fontWeight: 600,
+                lineHeight: "normal",
+                marginTop: "10px",
+              }}
+            >
+              {selectedBubbleIndex?.name}
+            </Text>
+            <Text
+              style={{
+                color: "--Cool-Dark-3, rgba(36, 41, 46, 0.60))",
+                textAlign: "center",
+                // fontFamily: 'Montserrat',
+                fontSize: "14px",
+                fontStyle: "normal",
+                fontWeight: 400,
+                lineHeight: "18px",
+                marginTop: "10px",
+              }}
+            >
+              {selectedBubbleIndex?.country ?? "United Arab Emirates, Dubai"}
+            </Text>
+            <Text
+              style={{
+                color: "var(--Cool-Dark-2, rgba(36, 41, 46, 0.85))",
+                textAlign: "center",
+                // fontFamily: 'Barlow',
+                fontSize: "14px",
+                fontStyle: "normal",
+                fontWeight: 400,
+                lineHeight: "18px",
+                marginTop: "18px",
+              }}
+            >
+              {selectedBubbleIndex?.brief?.split(" ")?.slice(0, 10)?.join(" ")}
+            </Text>
+            <Flex gap={10}>
+              <Text>
+                <span style={{ fontWeight: 600 }}>235</span> Connections |
+              </Text>
+              <Text>
+                <span style={{ fontWeight: 600 }}>35</span> Pipeline |{" "}
+              </Text>
+              <Text>
+                <span style={{ fontWeight: 600 }}>
+                  {selectedBubbleIndex.totalMeetings ?? 0}
+                </span>{" "}
+                Total Meetings
+              </Text>
+            </Flex>
 
-export default BubbleView
+            <Text style={{ color: "#FF681A" }}>
+              {selectedBubbleIndex.website ?? "Aladdib2b.com"}
+            </Text>
+
+            <Flex>
+              <Flex align="center">
+                <Avatar></Avatar> <Avatar></Avatar> <Avatar></Avatar>
+              </Flex>
+              <Flex vertical="vertical">
+                <Text>Alexandra Maldonado</Text>
+                <Text>Chief Executive Officer</Text>
+                <Text>Total 9 attendees</Text>
+              </Flex>
+            </Flex>
+            <Space direction="vertical" style={{ width: "100%" }}>
+              {/* <Flex gap="20px" vertical="vertical"> */}
+              <Button
+                size="large"
+                block
+                style={{ background: "#FF681A", color: "#fff" }}
+              >
+                <Text strong style={{ color: "#fff" }}>
+                  Request Meeting
+                </Text>
+              </Button>
+              <Button size="large" block>
+                <Text strong style={{ color: "#24292ED9" }}>
+                  Request for Quotation
+                </Text>
+              </Button>
+
+              <Button size="large" block>
+                <Text strong style={{ color: "#24292ED9" }}>
+                  View compay Profile
+                </Text>
+              </Button>
+              {/* </Flex> */}
+            </Space>
+          </div>
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default BubbleView;
